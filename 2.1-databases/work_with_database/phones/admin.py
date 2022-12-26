d31f7d3e8ad13ac5fde2dc6070930784e68a54e3
Sergej Mochalov <nouse4ano@gmail.com>
2022-12-26 23:54:00 +0300

    done Phones

--- 2.1-databases/work_with_database/phones/admin.py
+++ 2.1-databases/work_with_database/phones/admin.py
@@ -5,4 +5,5 @@ from phones.models import Phone
 @admin.register(Phone)
 class PhoneAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'image', 'price', 'release_date', 'lte_exists', 'slug',]
-    list_filter = ['name', 'price', 'release_date']
+    list_filter = ['name', 'price', 'release_date',]
+    prepopulated_fields = {'slug': ['name',]}
