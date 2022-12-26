d75e271841a06830ea86ff350ea0f2affcb42bf4
Sergej Mochalov <nouse4ano@gmail.com>
2022-12-26 22:32:48 +0300

    done Phones

--- 2.1-databases/work_with_database/phones/admin.py
+++ 2.1-databases/work_with_database/phones/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from phones.models import Phone
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'image', 'price', 'release_date', 'lte_exists', 'slug',]
+    list_filter = ['name', 'price', 'release_date']
