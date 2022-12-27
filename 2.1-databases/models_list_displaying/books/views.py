38ad8f6380aa33b03ddbed6766a6b71c533c4412
Sergej Mochalov <nouse4ano@gmail.com>
2022-12-28 01:10:07 +0300

    done Books

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -16,14 +16,19 @@ def book_scope(request, pub_date):
     sorted_books_objects = Book.objects.all().order_by('pub_date')
     paginator = Paginator(sorted_books_objects, 1)
     context = {}
+
     for page, book in enumerate(sorted_books_objects, 1):
         if pub_date == book.pub_date:
-            page = paginator.page(page)
-            if page.has_previous():
-                prev_page_by_date = paginator.page(page.previous_page_number()).object_list[0].pub_date
+            page_by_date = paginator.page(page)
+
+            if page_by_date.has_previous():
+                prev_page_by_date = paginator.page(page_by_date.previous_page_number()).object_list[0].pub_date
                 context['prev_page'] = prev_page_by_date
-            if page.has_next():
-                next_page_by_date = paginator.page(page.next_page_number()).object_list[0].pub_date
+
+            if page_by_date.has_next():
+                next_page_by_date = paginator.page(page_by_date.next_page_number()).object_list[0].pub_date
                 context['next_page'] = next_page_by_date
+
             context['book'] = book
+
     return render(request, template, context)
